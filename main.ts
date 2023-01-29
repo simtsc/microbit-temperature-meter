75d9e80534cbb10b6c5a6d63db8e31c3e0f91486
simtsc <2539637+simtsc@users.noreply.github.com>
2023-01-29 22:11:59 +0100

    added sound/temp level toggel

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+let maxval = 0
 let reading = 0
+let toggle = true
 basic.forever(function () {
-    reading = input.temperature()
+    if (input.buttonIsPressed(Button.B)) {
+        toggle = !(toggle)
+        basic.pause(500)
+    }
+    if (toggle) {
+        reading = input.temperature()
+        maxval = 50
+    } else {
+        reading = input.soundLevel()
+        maxval = 255
+    }
     led.plotBarGraph(
     reading,
-    100
+    maxval
     )
     if (input.buttonIsPressed(Button.A)) {
         basic.clearScreen()
